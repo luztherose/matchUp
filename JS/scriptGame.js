07b6c10bcdcab286a77ffc600f3a04ff1eba03da
Luz De La Rosa <luzdelarosaramirez@gmail.com>
2020-05-03 21:26:14 -0400

    Closes issue #3 Adding a new functionality

--- JS/scriptGame.js
+++ JS/scriptGame.js
@@ -2,11 +2,14 @@ let images = document.getElementsByTagName("img");
 let toggleButton = document.getElementById("toggleButton");
 let heading = document.querySelector("h1");
 let audioSound = document.getElementById("myAudio");
+let matchSound = document.getElementById("matchSound");
 let bodyTag = document.querySelector("body");
 const defaultColor = "rgb(240, 11, 11)";
 const bodyBackgroundColor = "#333";
+let firstImageClicked; 
 
 window.onload = init;
+
 function init() {
     let images = document.getElementsByTagName("img");
     for (let i = 0; i < images.length; i++) {
@@ -15,16 +18,31 @@ function init() {
 };
 
 function revealImage(eventObject) {
-    let image = eventObject.target; // the target tells what element generated the event
-        image.style.filter = "none";
-        audioSound.play();
-        setTimeout(brightnessOut, 3000, image);
+    let clikedImage = eventObject.target; // the target tells what element generated the event
+    clikedImage.style.filter = "none";
+    
+    let myFunction = function(){
+        if (firstImageClicked == undefined) {
+            firstImageClicked = clikedImage;
+        }else {
+            if (firstImageClicked.src == clikedImage.src) {
+                matchSound.play();
+            } else {
+                audioSound.play();
+                brightnessOut(firstImageClicked)
+                brightnessOut(clikedImage)
+            }
+            firstImageClicked = undefined;
+        }
+    }
+    setTimeout(myFunction , 400);
 }
 
+
 function brightnessOut(image) {
-    let name;
-    name = image.style.filter = "brightness(0%)";
+    image.style.filter = "brightness(0%)";
 }
+
 function revealImages() {
     for(let i = 0; i < images.length; i++) {
         images[i].style.filter = "none";
@@ -48,3 +66,12 @@ toggleButton.addEventListener("click", () => {
         hideImages();
     }
 });
+
+// 1) Reveal first image
+// Store the first image 
+// 2) Reveal a second image
+// Store the second  image 
+// 3) Compare first and second images 
+// if they are equal both images stay revealed 
+// else both images get closed
+
