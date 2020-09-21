16c2dfb76b1435f3b05d61d942413619d476a009
luztherose <luzdelarosaramirez@gmail.com>
2020-09-20 20:23:10 -0400

    refactoring code

--- JS/userReview.js
+++ JS/userReview.js
@@ -1,14 +1,18 @@
 $('.review').fadeIn();
 $('#flashMessage').slideDown(1000).delay(2000).slideUp(1000);
-$('#submitBtn').click(function () {
-    const userName = $('#userNameInput').val();
-    const gameReview = $('#gameReviewInput').val();
-    const h3Heading = $("<h3></h3>").text(userName);
-    const paragraphReview = $("<p></p>").text(gameReview);
-    $('#newReviews').append(h3Heading);
-    $('#newReviews').append(paragraphReview);
-    $('#userNameInput').val("");
-    $('#gameReviewInput').val("");
-    $('#userNameInput').focus();
-});
 
+setSubitButton();
+
+const setSubitButton = () => {
+    $('#submitBtn').click(function () {
+        const userName = $('#userNameInput').val();
+        const gameReview = $('#gameReviewInput').val();
+        const h3Heading = $("<h3></h3>").text(userName);
+        const paragraphReview = $("<p></p>").text(gameReview);
+        $('#newReviews').append(h3Heading);
+        $('#newReviews').append(paragraphReview);
+        $('#userNameInput').val("");
+        $('#gameReviewInput').val("");
+        $('#userNameInput').focus();
+    });
+}
